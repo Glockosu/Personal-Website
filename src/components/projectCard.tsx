b02bfb5110e88abd46d86393d46c6bac1b4301ff
Glockosu <glockbhop@gmail.com>
2024-03-05 01:07:37 -0500

    Image changes
    Image styling changes for more responsive design.

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 import { FiGithub } from 'react-icons/fi'; // Ensure you have react-icons installed
 
 const ProjectCard = ({ title, imageSrc, githubLink, skills }) => {
     return (
         <div className="max-w-full bg-gray-900 rounded-lg border border-gray-700 shadow-md hover:scale-105 transition-transform duration-300 ease-in-out purple-tint">
-        <div className="">
-            <Image className="rounded-t-lg" src={imageSrc} alt={title} width={300} height={125} layout="responsive" />
+        <div className="image-container" style={{ position: 'relative', width: '100%', height: '0', paddingBottom: '41.66%' }}>
+            <img className="rounded-t-lg" src={imageSrc} alt={title} style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'cover' }} />
         </div>
             <div className="p-5">
             <h5 className="text-white text-2xl font-bold tracking-tight mb-2">{title}</h5>
