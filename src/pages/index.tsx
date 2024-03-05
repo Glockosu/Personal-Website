71d5f46a5179b2065374bd313c69a1a844164831
Glockosu <glockbhop@gmail.com>
2024-03-05 00:48:27 -0500

    Image Path Changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ function redirectToLinkedIn() {
 export default function Home() {
   const projects = [
     {
-      image: '/../images/projects/mimibot-app.png', // Replace with your project image path
+      image: './projects/mimibot-app.png', // Replace with your project image path
       title: 'MimiBot App',
       description: 'A Node.js-based Discord bot, designed to elevate the Tower of Fantasy gameplay experience by offering instant access to character data and analytics. Features integration with the Tower of Fantasy Simulacra API for real-time character insights and statistical analysis, leveraging asynchronous programming for swift and accurate data handling. Equipped with sophisticated algorithms to calculate potential damage outputs for various character combinations, all accessible through user-friendly bot commands.',
       githubUrl: 'https://github.com/Glockosu/mimibot',
@@ -51,7 +51,7 @@ export default function Home() {
       <div className="sidebar">
         <aside className="sticky top-0 pt-20 pl-0 h-screen flex justify-center md:pl-5 w-128 ">
           <div className="text-center">
-            <Image src="/../images/profile-pic-nice.webp" alt="Alex Johannesson" width={200} height={200} className="mx-auto rounded-md transition-transform transition-filter grayscale hover:grayscale-0 hover:scale-105" />
+            <Image src="./profile-pic-nice.webp" alt="Alex Johannesson" width={200} height={200} className="mx-auto rounded-md transition-transform transition-filter grayscale hover:grayscale-0 hover:scale-105" />
             <h1 className="text-2xl font-bold mt-4">Alex Johannesson</h1>
             <p className="text-gray-400 mt-4">Software Engineer</p>
             <p className="text-gray-400 mt-2">I build cool stuff, check out my projects below :)</p>
